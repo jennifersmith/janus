e3569f6be2b1540c8fe650edd8fa5be518df5d62
Jennifer Smith <jennifer@home>
2013-09-14 23:34:24 +0100

    bumped some stuff

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject janus "0.0.2"
   :description "Consumer-driven contracts, verified both ways."
   :url "http://github.com/gga/janus"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/data.json "0.1.1"]
                  [json-path "0.2.0"]
                  [clj-http "0.3.2"]
@@ -9,7 +9,6 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/data.xml "0.0.3"]]
   :profiles {:dev {:dependencies [ [clj-http-fake "0.2.3"]
-                                   [midje "1.3.1"]]
-                   :plugins [[swank-clojure "1.4.0-SNAPSHOT"]
-                             [lein-midje "2.0.1"]]}}
+                                   [midje "1.6-beta1"]]
+}}
   :main janus)
