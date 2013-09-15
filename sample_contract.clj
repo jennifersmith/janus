494230a8093894568f098e93b4cac363487677ae
Jennifer Smith <jennifer@home>
2013-09-15 10:23:14 +0100

    Updated sample_contract to a valid contract syntax

--- sample_contract.clj
+++ sample_contract.clj
@@ -1,43 +1,9 @@
-(environment :dev (fn []))
+(service "simple JSON service"
+      (contract "GET document"
+        (method :get)
+        (url "http://localhost:4568/service")
+        (header "Content-Type" "application/json")
 
-(service
- (method :post)
- (header "Content-Type" "application/json")
+        (should-have :path "$.id" :of-type :number)
+        (should-have :path "$.features[*]" :matching #"[a-z]")))
 
- (before (fn [config]
-           ;; Perform a POST to authenticate, for example
-
-           (add-cookie config "SESSION" (-> :cookie-jar response (get "SESSION_ID")))
-           ))
-
- (after (fn []))
-
- (contract
-  "payment recipient list"
-  (definition
-    (url "/recipients"))
-
-  (clauses
-   (should_have :path "$..recipients" :of_type :array)
-   (should_have :path "$..recipients[*].name", :equal_to "")
-
-   (should_have :path "$.name", :matching #"\w+\w+")))
-
- (conversation
-  "Creating a new send money transaction"
-
-  (contract
-   "create transaction"
-   :named submit_tx
-   (definition ...)
-   (clauses ...))
-
-  (contract
-   "verify transaction details"
-   (definition
-     (url "...")
-     (body {
-            :transactionid (:transaction_id submit_tx)}))
-
-   (clauses
-    (should_have :path "", :of_type :string)))))
