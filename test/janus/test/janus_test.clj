00b0a5c14a33ce4b198c15774be8ea2bf38c5122
Jennifer Smith <jennifer@home>
2013-09-24 08:28:55 +0100

    First rewrite of the features to midje

---
+++ test/janus/test/janus_test.clj
@@ -0,0 +1,39 @@
+(ns janus.test.janus-test
+  (:import [java.io Closeable])
+  (:require [janus]
+            [janus.dsl :refer [service contract method url header should-have]]
+            [midje.sweet :refer :all]
+            [ring.util.serve :refer [serve* stop-server]]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response]]))
+
+
+
+(defn handler [request]
+  (response {:foo "bar"}))
+
+(def app
+  (wrap-json-response handler))
+
+(defn create-handler [response-body]
+  (fn [request]
+    (response response-body)))
+
+(defn serve-response [response-body]
+  (serve* (wrap-json-response
+          (create-handler response-body))
+          8080 true))
+
+
+(against-background [(before :facts
+                             (serve-response {:id "1" :features ["a" "b"]}))]
+  (facts "Can verify a single contract for a running service"
+    (janus/unsafe-verify
+     '(service "simple JSON service"
+               (contract "GET document"
+                         (method :get)
+                         (url "http://localhost:8080/")
+                         (header "Content-Type" "application/json")
+                         (should-have :path "$.id" :of-type :number)
+                         (should-have :path "$.features[*]" :matching #"[a-z]")))
+     ) => true))
